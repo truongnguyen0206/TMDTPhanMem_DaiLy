caa45e3d96ea02c3ca8cd675a45fdccd8e891534
TrungThanh-Coffee <thanh.tt18824@sinhvien.hoasen.edu.vn>
2025-12-09 16:34:58 +0700

    Sửa lại xuất báo cáo

--- backend/src/api/routes/report_route.js
+++ backend/src/api/routes/report_route.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const reportController = require("../../controllers/report_controller");
+const { authenticateToken } = require('../../middlewares/auth_middleware');
 
 // Xuất Excel
-router.get("/orders/excel", reportController.exportOrdersExcel);
+router.get("/orders/excel/:user_id", reportController.exportOrdersExcel);
 
 // Xuất PDF
-router.get("/orders/pdf", reportController.exportOrdersPDF);
+router.get("/orders/pdf/:user_id", reportController.exportOrdersPDF);
 
 module.exports = router;
